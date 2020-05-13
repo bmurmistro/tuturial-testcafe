dbc5d6e8737fbbc9b6d93615965a521cd81ceb5e
artem <tranduil15@gmail.com>
2020-05-13 19:50:19 +0300

    Updated tests to the java VG example Updated dependencies and readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Tetscafe tutorial
+### Pre-requisites:
+1. Install Node.js from [here](https://nodejs.org/en/)
 
-<!-- Please see https://applitools.com/tutorials/testcafe.html for all the details. -->
+### Running the example:
+1. Download the example
+    * Option 1: `git clone https://github.com/applitools/tutorial-testcafe.git`
+    * Option 2: Download it as a Zip file and extract it
+2. CD into the `tutorial-testcafe` folder
+3. Set up environment variable `APPLITOOLS_API_KEY` with your own API key.
+    * Login to Applitools > Click on the Person icon > My API Key
+    * Linux/Mac: export APPLITOOLS_API_KEY=<your_key>
+    * Windows: set APPLITOOLS_API_KEY=<your_key>
+4. run `npm install`
+5. run `npm test`
+
+### Tetscafe tutorial
+
+Please see https://applitools.com/tutorials/testcafe.html for all the details.
+
+### NPM docs
+The following NPM docs contains all the update information for this SDK
+https://www.npmjs.com/package/@applitools/eyes-testcafe#Best-practice-for-using-the-SDK
+
--- tests/play.testcafe.js
+++ tests/play.testcafe.js
@@ -2,22 +2,47 @@
 /// <reference types="@applitools/eyes-testcafe" />
 
 import Eyes from '@applitools/eyes-testcafe';
+
+// Initialize the eyes
 const eyes = new Eyes();
 
-fixture`My Testcafe Hello world`.page`https://demo.applitools.com`
-  .afterEach(async () => eyes.close())
-  .after(async () => await eyes.waitForResults());
+// Set page used in the test
+fixture`Testcafe Demo App`.page`https://demo.applitools.com`
+    .afterEach(async () => eyes.close()) // Closing eyes after the test
+    .after(async () => await eyes.waitForResults());
 
 test('Tetscafe Helloworld', async t => {
+    // Start the test by setting AUT's name, test name and viewport size (width X height)
     await eyes.open({
-      t, // pass testcafe contorller
-      appName: 'My TestCafe App',
-      testName: 'Testcafe Helloworld',
-      browser: [
-        { width: 1024, height: 768, name: 'chrome' },
-        { width: 800, height: 600, name: 'firefox' },
-        { deviceName: 'iPhone X', screenOrientation: 'landscape' }
-      ],
+        t, // pass testcafe contorller
+        appName: 'Demo App',
+        testName: 'Ultrafast grid demo',
+        browser: [
+            {width: 800, height: 600, name: 'chrome'},
+            {width: 700, height: 500, name: 'firefox'},
+            {width: 1600, height: 1200, name: 'ie11'},
+            {width: 1024, height: 768, name: 'edgechromium'},
+            {width: 800, height: 600, name: 'safari'},
+            {deviceName: 'iPhone X', screenOrientation: 'portrait'},
+            {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
+        ],
+        batchName: 'Ultrafast Batch'
     });
-    await eyes.checkWindow('first page');
+
+    // Visual checkpoint #1 - Check the login page.
+    await eyes.checkWindow({
+        tag: "Login Window",
+        target: 'window',
+        fully: true
+    });
+
+    // This will create a test with two test steps.
+    await t.click('#log-in')
+
+    // Visual checkpoint #2 - Check the app page.
+    await eyes.checkWindow({
+        tag: "App Window",
+        target: 'window',
+        fully: true
+    })
 });
