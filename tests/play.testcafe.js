89018e32f2a31172eb5c50e62c2f96f23ea4e96e
artem <tranduil15@gmail.com>
2020-05-20 15:27:56 +0300

    updated after hook Added comments

--- tests/play.testcafe.js
+++ tests/play.testcafe.js
@@ -8,21 +8,32 @@ const eyes = new Eyes();
 
 // Set page used in the test
 fixture`Testcafe Demo App`.page`https://demo.applitools.com`
-    .afterEach(async () => eyes.close()) // Closing eyes after the test
-    .after(async () => await eyes.waitForResults());
+    // Closing eyes after the test
+    .afterEach(async () => eyes.close())
+    .after(async () => {
+        // Wait and collect all test results
+        // we pass false to this method to suppress the exception that is thrown if we
+        // find visual differences
+        let allTestResults = await eyes.waitForResults(false)
+        // Print results
+        console.log(allTestResults)
+    });
 
-test('Tetscafe Helloworld', async t => {
-    // Start the test by setting AUT's name, test name and viewport size (width X height)
+test('ultraFastTest', async t => {
+    // Start the test by setting AUT's name, test name and batch name
     await eyes.open({
         t, // pass testcafe contorller
         appName: 'Demo App',
         testName: 'Ultrafast grid demo',
+        //
         browser: [
+            // Add browsers with different viewports
             {width: 800, height: 600, name: 'chrome'},
             {width: 700, height: 500, name: 'firefox'},
             {width: 1600, height: 1200, name: 'ie11'},
             {width: 1024, height: 768, name: 'edgechromium'},
             {width: 800, height: 600, name: 'safari'},
+            // Add mobile emulation devices in Portrait mode
             {deviceName: 'iPhone X', screenOrientation: 'portrait'},
             {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
         ],
